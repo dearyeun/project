2622997b7d8991235e61ec0d036abd401572e742
dearyeun <dearyeun@naver.com>
2024-01-14 21:56:08 +0900

    2일차 과외

--- test.py
+++ test.py
@@ -1,1 +1,42 @@
-print("hello")
+################################################################### 2024.01.14
+# a=[1,2.5,"a",True,[1,2]]
+# b=[[[]]]
+# print(a)
+
+# len(a[4])
+
+# a={'test':'python'}
+# print(a)
+# print(a['test'])
+# a['list']=[1,5.5]
+# print(a)
+# print(a['list'])
+
+# test=[1,2,2,5,1,6,7,6,0,0,2,2]
+# temp=list(set(test))
+# print(test)
+# print(temp)
+
+# temp란 변수에 숫자를 입력받아서 짝수면 "짝수입니다." 홀수면 "홀수입니다."를 출력하시오
+# temp =int(input())
+# if temp %2==0:
+#     print("짝수입니다")
+# else:
+#     print("홓수입니다")
+
+
+# import datetime as d
+# curr1=d.datetime.now()
+# temp=[0]*10000000
+# print("length = ",len(temp))
+# for i in range(len(temp)):
+#     pass
+# curr2=d.datetime.now()
+
+# print(curr1)
+# print(curr2)
+################################################################### 2024.01.14
+
+
+
+
